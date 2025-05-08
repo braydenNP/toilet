a355a53b32c56d1b3fefb39e7008d29c351293a0
braydenNP <s10258659@connect.np.edu.sg>
2025-05-08 21:50:23 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+body = document.querySelector("body");
+lightSwitch = document.querySelector(".light-switch");
+lightSwitch.addEventListener("click", function(){
+    body.classList.toggle("lights-off")
+})
+rightDoor = document.querySelector(".right-door");
+rightDoor.addEventListener("click", function(){
+    body.classList.toggle("door-open")
+})
+
+leftDoor = document.querySelector(".left-door");
+leftDoor.addEventListener("click", function(){
+    if (body.classList.contains("door-open")){
+        body.classList.toggle("door-open2")
+    }
+})
