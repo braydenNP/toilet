bf911a1869ada06912d8126f1410ab34792b9656
braydenNP <s10258659@connect.np.edu.sg>
2025-05-13 23:06:54 +0800

    The toilet can open

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ body = document.querySelector("body");
 lightSwitch = document.querySelector(".light-switch");
 lightSwitch.addEventListener("click", function(){
     body.classList.toggle("lights-off")
+
 })
 rightDoor = document.querySelector(".right-door");
 rightDoor.addEventListener("click", function(){
@@ -14,3 +15,8 @@ leftDoor.addEventListener("click", function(){
         body.classList.toggle("door-open2")
     }
 })
+
+const toilet = document.querySelector('.toilet-container');
+toilet.addEventListener("click", function() {
+    toilet.classList.toggle("open-toilet");
+});
